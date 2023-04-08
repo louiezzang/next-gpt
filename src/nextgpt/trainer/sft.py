49b41683f2c7a59a8d7660dd10c114e384e3c607
Younggue Bae <ybae2@lululemon.com>
2023-04-08 13:09:51 -0700

    Correct mistypo

--- src/nextgpt/trainer/sft.py
+++ src/nextgpt/trainer/sft.py
@@ -44,7 +44,7 @@ class SFTTrainer(ABC):
         eval_dataloader: DataLoader = None,
         batch_size: int = 1,
         max_epochs: int = 2,
-        accimulation_steps: int = 8,
+        accumulation_steps: int = 8,
     ) -> None:
         super().__init__()
         self.strategy = strategy
@@ -57,8 +57,8 @@ class SFTTrainer(ABC):
             self.model = self.model.module
         self.optimizer = strategy.setup_optimizer(optim, self.model)
 
-        self.accimulation_steps = accimulation_steps
-        num_update_steps_per_epoch = len(train_dataloader) // self.accimulation_steps
+        self.accumulation_steps = accumulation_steps
+        num_update_steps_per_epoch = len(train_dataloader) // self.accumulation_steps
         max_steps = math.ceil(self.epochs * num_update_steps_per_epoch)
 
         self.scheduler = get_scheduler("cosine",
@@ -71,7 +71,7 @@ class SFTTrainer(ABC):
         # wandb.watch(self.model)
         total_loss = 0
         # epoch_bar = tqdm(range(self.epochs), desc='Epochs', disable=not is_rank_0())
-        step_bar = tqdm(range(len(self.train_dataloader) // self.accimulation_steps * self.epochs),
+        step_bar = tqdm(range(len(self.train_dataloader) // self.accumulation_steps * self.epochs),
                         desc=f'steps',
                         disable=not is_rank_0())
         for epoch in range(self.epochs):
@@ -97,26 +97,26 @@ class SFTTrainer(ABC):
                     # logger.warning(f"batch_id:{batch_id}, abnormal loss: {loss}")
                     print(f"batch_id:{batch_id}, abnormal loss: {loss}")
 
-                loss = loss / self.accimulation_steps
+                loss = loss / self.accumulation_steps
 
                 self.strategy.backward(loss, self.model, self.optimizer)
 
                 total_loss += loss.item()
 
                 # gradient accumulation
-                if (batch_id + 1) % self.accimulation_steps == 0:
+                if (batch_id + 1) % self.accumulation_steps == 0:
                     self.strategy.optimizer_step(self.optimizer)
                     self.optimizer.zero_grad()
                     self.scheduler.step()
                     # wandb.log({
-                    #     "loss": total_loss / self.accimulation_steps,
+                    #     "loss": total_loss / self.accumulation_steps,
                     #     "lr": self.scheduler.get_last_lr()[0],
                     #     "epoch": epoch,
                     #     "batch_id": batch_id
                     # })
                     step_bar.update()
                     step_bar.set_postfix({
-                        "loss": total_loss / self.accimulation_steps,
+                        "loss": total_loss / self.accumulation_steps,
                         "lr": self.scheduler.get_last_lr()[0],
                         "epoch": epoch,
                         "batch_id": batch_id
