e4d66e79e5dd919724c9b0035b206f50d5b68b30
Younggue Bae <ybae2@lululemon.com>
2023-04-07 23:58:36 -0700

    Updated __init__

--- src/nextgpt/dataset/__init__.py
+++ src/nextgpt/dataset/__init__.py
@@ -1,4 +1,8 @@
 from .reward_dataset import RewardDataset
+from .sft_dataset import DataCollatorForSupervisedDataset, SFTDataset, SupervisedDataset
 from .utils import is_rank_0
 
-__all__ = ['RewardDataset', 'is_rank_0']
+__all__ = [
+    'RewardDataset', 'is_rank_0', 'SFTDataset', 'SupervisedDataset',
+    'DataCollatorForSupervisedDataset'
+]
