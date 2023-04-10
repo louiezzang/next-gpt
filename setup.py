ea24cd0165443d0072c90bab6e90a1ad263ac51e
Younggue Bae <ybae2@lululemon.com>
2023-04-10 13:08:00 -0700

    Bug fix of setup script

--- setup.py
+++ setup.py
@@ -3,28 +3,21 @@
 """
 import os
 import sys
+import pkg_resources
 from setuptools import setup, find_packages
 
-here = os.path.dirname(os.path.realpath(__file__))
 
-def read_file(filename: str):
-    try:
-        lines = []
-        with open(filename) as file:
-            lines = file.readlines()
-            lines = [line.rstrip() for line in lines if not line.startswith('#')]
-        return lines
-    except:
-        return []
+VERSION = "0.0.2"
+DESCRIPTION = "nextGPT"
 
 setup(
     name="nextgpt",
-    version="0.0.1",
-    description="nextGPT",
+    version=VERSION,
+    description=DESCRIPTION,
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Younggue Bae",
-    # author_email="louiezzang@gmail.com",
+    # author_email="<louiezzang@gmail.com>",
     package_dir={"": "src"},
     url="https://github.com/louiezzang/next-gpt",
     keywors=[
@@ -33,7 +26,12 @@ setup(
         "LLaMA",
     ],
     packages=find_packages(where="src"),
-    install_requires=read_file(f"{here}/requirements.txt"),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
     python_requires=">=3.6",
     include_package_data=True,
 )
