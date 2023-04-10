f6e449b859b3d5ab1e28e65541e4ace67ed4f688
Younggue Bae <ybae2@lululemon.com>
2023-04-10 16:49:17 -0700

    Added log metrics callback

--- src/nextgpt/trainer/callbacks/__init__.py
+++ src/nextgpt/trainer/callbacks/__init__.py
@@ -1,5 +1,6 @@
 from .base import Callback
 from .performance_evaluator import PerformanceEvaluator
 from .save_checkpoint import SaveCheckpoint
+from .log_metrics import LogMetrics
 
-__all__ = ['Callback', 'PerformanceEvaluator', 'SaveCheckpoint']
+__all__ = ['Callback', 'PerformanceEvaluator', 'SaveCheckpoint', 'LogMetrics']
